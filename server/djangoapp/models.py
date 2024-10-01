0729bf87c4f44510edea96ab7fb02d81628443b6
Abdulkadir Abubakar <abdulkaadiras@gmail.com>
2024-10-01 15:37:09 +0100

    Updated model.py, populate.py, urls.py, views.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-#from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -48,6 +48,6 @@ class CarModel(models.Model):
             MinValueValidator(2015)
         ]
     )
-        
+
     def __str__(self):
         return self.name  # Return the name as the string representation
