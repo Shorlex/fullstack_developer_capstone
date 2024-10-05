7079c05f685b633f6bcb71dcc4d8f06b44615dd7
Abdulkadir Abubakar <abdulkaadiras@gmail.com>
2024-10-05 15:59:31 -0400

    Created Car Inventory

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,8 +14,8 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
-from .restapis import get_request, analyze_review_sentiments
-from .restapis import get_request, analyze_review_sentiments, post_review, searchcars_request
+from .restapis import get_request, analyze_review_sentiments,
+searchcars_request
 
 
 # Get an instance of a logger
@@ -184,8 +184,7 @@ def get_inventory(request, dealer_id):
         elif 'price' in data:
             endpoint = "/carsbyprice/"+str(dealer_id)+"/"+data['price']
         else:
-            endpoint = "/cars/"+str(dealer_id)
- 
+            endpoint = "/cars/"+str(dealer_id) 
         cars = searchcars_request(endpoint)
         return JsonResponse({"status": 200, "cars": cars})
     else:
